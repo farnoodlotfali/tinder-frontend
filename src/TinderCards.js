a989baa6bd15ecec4bb4296ee684e01dc5aedf57
farnoodlotfali <farnoodlotfali@gmail.com>
2020-12-21 20:17:50 +0330

    axios

--- src/TinderCards.js
+++ src/TinderCards.js
@@ -1,34 +1,22 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import TinderCard from 'react-tinder-card';
 import './TinderCards.css';
+import axios from './axios';
 
 const TinderCards = () => {
-  const [lastDir, setLastDir] = useState();
-  const [people, setPeople] = useState([
-    {
-      name: 'Hector Salamanca',
-      url:
-        'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSsoL7u7vLAj-TVEB6dZ74oikGK12Kr0OZVBQ&usqp=CAU',
-    },
-    {
-      name: 'Hector Salamanca',
-      url:
-        'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRpTN60phjjjq_Bahjg2asXgjEZ9XxhTJdMDA&usqp=CAU',
-    },
-    {
-      name: 'Hector Salamanca',
-      url:
-        'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQYAHZ0lpBtlP8lXebTIEtnA5-gDxLrPaL-CA&usqp=CAU',
-    },
-    {
-      name: 'Hector Salamanca',
-      url:
-        'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTkvN0Uuk8hJ9E7VREWI6ntn_WAdkmEEQd5AA&usqp=CAU',
-    },
-  ]);
+  const [people, setPeople] = useState([]);
+  useEffect(() => {
+    const fetchData = async () => {
+      const req = await axios.get('/tinder/cards');
+      setPeople(req.data);
+    };
+
+    fetchData();
+  }, []);
+
+  console.log(people);
   const swiped = (dir, nameToDelete) => {
     console.log(nameToDelete);
-    setLastDir(dir);
   };
   const outOfFrame = (name) => {
     console.log(name);
@@ -46,7 +34,7 @@ const TinderCards = () => {
           >
             <div
               className='card'
-              style={{ backgroundImage: `url( ${person.url})` }}
+              style={{ backgroundImage: `url( ${person.imgUrl})` }}
             >
               <h3>{person.name}</h3>
             </div>
